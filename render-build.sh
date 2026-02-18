21064bdc0f2175fe36028dfea930815277935c3a
Hihla <97955564+Hihla@users.noreply.github.com>
2026-02-19 01:48:30 +0300

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# تثبيت المكتبات
 pip install -r requirements.txt
+
+# تحميل ملفات المتصفح المطلوبة (هذا هو السطر الناقص!)
 playwright install chromium
